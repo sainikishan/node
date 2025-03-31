0ea0b15040a03f9b8c648eda48197948b78cf536
all uodate <kishansaini666@gmail.com>
2025-03-31 20:10:15 +0530

    node asyncread file

--- fsmodulesasync.js
+++ fsmodulesasync.js
@@ -13,4 +13,13 @@ fs.writeFile(filepath, "this is the inital data", "utf-8", (err) => {
         console.log("file created");
     }
 });
+//read file
+fs.readFile(filepath,  "utf-8", (err,data) => {
+    if (err) {
+        console.error(err);
+    }
+    else {
+        console.log(data);
+    }
+});
 
