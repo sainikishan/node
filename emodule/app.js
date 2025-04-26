0c84218b8137b69e0f0d04e21e20d56b884f21b9
all uodate <kishansaini666@gmail.com>
2025-04-26 09:26:15 +0530

    work

---
+++ emodule/app.js
@@ -0,0 +1,7 @@
+// Modules
+// const add = require("./math");
+import mult from "./math.js";
+// const {add,multi}=require("./math");
+// console.log(add(5, 10));
+console.log(mult(5, 10));
+// commonjs
---
+++ emodule/math.js
@@ -0,0 +1,13 @@
+const add = (a, b) =>
+{
+    return a + b ;
+}
+const mult = (a, b) =>
+{
+    return a * b ;
+}
+
+// module.exports.add = add;
+// module.exports.multi = mult;
+
+export default mult;
